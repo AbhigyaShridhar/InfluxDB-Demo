06a678e40457a3a712588fef17f3d97944d033da
AbhigyaShridhar <55044948+AbhigyaShridhar@users.noreply.github.com>
2022-04-12 19:58:27 +0530

    Update influxdb2.py

--- influxdb2.py
+++ influxdb2.py
@@ -1,5 +1,6 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.domain.patch_retention_rule import PatchRetentionRule
 
 url = "http://localhost:8086"
 bucket = "openwisp2"
@@ -26,3 +27,11 @@ write_api.write(bucket=bucket, record=point)
 query = '''from(bucket:"openwisp2") |> range(start: 0, stop: now()) |> filter(fn: (r) => r._measurement == "test_load_short")'''
 records = query_api.query_stream(query)
 # records is a 'generator object and needs to be modified if rpresentation is desired in the form of a dictionary or a json object'
+
+# get the object for bucket with it's name
+bucket = client.buckets_api().find_bucket_by_name("openwisp2")
+
+#retention rules
+retention_rules = bucket.retention_rules
+
+#PatchRetentionRule for more precise control over each retention rule
